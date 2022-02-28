61e1e06777a8455ec8f1533eb3b4a4d8f0f5b29e
Narsingh9 <tamminananarsingh@gmail.com>
2022-02-28 11:33:06 +0530

    Added client  basic css js and landing page

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Landing from "./pages/Landing";
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>HELLO WORLD</h1>
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
