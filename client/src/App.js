fc7d5779d482fb364bd5cf03bf7c0b83c8547fb2
Narsingh9 <tamminananarsingh@gmail.com>
2022-02-28 12:26:48 +0530

    added landing wrapper to landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import Landing from "./pages/Landing";
+import styled from "styled-components";
+
+// const Button = styled.button`
+//   background: red;
+//   color: white;
+//   font-size: 1rem;
+// `;
 
 function App() {
   return (
     <div className="App">
-      
-      <h1>HELLO WORLD</h1>
       <Landing />
     </div>
   );
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,11 +1,33 @@
-import React from 'react'
-import logo from '../assets/images/logo.svg';
-import main from '../assets/images/main.svg';
+import React from "react";
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
 
 const Landing = () => {
-return (
-    <div><h4>Landing Page</h4></div>
-)
-}
+    return (
+        <Wrapper>
+            <nav>
+                <img src={logo} alt="jobify" className="logo" />
+            </nav>
+            <div className="container page">
+                <div className="info">
+                    <h1>
+                        job <span>tracking</span> app
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amex, consecrator dehiscing elis, sed do
+                        gismo temporal incididunt ut Lahore et dolore magna aliqua. Ut enim
+                        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate veil esse chillums dolor eu fujian sulla
+                        pariatur.
+                    </p>
+                    <button className="btn btn-hero">Login/register</button>
+                </div>
+                <img src={main} alt="job hunt" className="img main-img"></img>
+            </div>
+        </Wrapper>
+    );
+};
 
-export default Landing
+export default Landing;
