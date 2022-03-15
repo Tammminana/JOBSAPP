322020fafab2c1f7b6252f0ca58cea656a45bea2
Narsingh9 <tamminananarsingh@gmail.com>
2022-03-15 23:04:37 +0530

    login user in the server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
 import 'express-async-errors';
+import morgan from 'morgan';
 
 //db and authenticate the User
 import connectDB from "./db/connect.js";
@@ -18,6 +19,9 @@ import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 
 // app.use(cors());
+if (process.env.NODE_ENV !== 'production'){
+    app.use(morgan('dev'));
+}
 app.use(express.json());
 
 app.get("/", (req,res) => {
