9fae87078cccd6ae6a482374e10d84003e2fbf5b
Narsingh9 <tamminananarsingh@gmail.com>
2022-04-21 14:06:50 +0530

    profile page setup

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -94,8 +94,12 @@ const AppProvider = ({ children }) => {
         removeUserFromLocalStorage();
     }
 
+    const updateUser = async (currentUser) =>{
+        console.log(currentUser)
+    }
+
     return <AppContext.Provider
-        value={{ ...state, displayAlert, setupUser, toggleSidebar, logoutUser }}>
+        value={{ ...state, displayAlert, setupUser, toggleSidebar, logoutUser,updateUser }}>
         {children}
     </AppContext.Provider>
 }
